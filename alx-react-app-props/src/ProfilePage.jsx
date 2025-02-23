e4db23092042faa21507cc99496b09c0f725574c
prince joseph udoewah <princepiper28@gmail.com>
2025-02-23 11:16:31 +0100

    Removed unnecessary props from ProfilePage.jsx and UserInfo.jsx

---
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -0,0 +1,7 @@
+import UserInfo from './UserInfo';
+
+function ProfilePage() {
+  return <UserInfo />; // No longer passing userData as a prop
+}
+
+export default ProfilePage;
---
+++ alx-react-app-props/src/UserInfo.jsx
@@ -0,0 +1,7 @@
+import UserDetails from './UserDetails';
+
+function UserInfo() {
+  return <UserDetails />; // No longer passing userData as a prop
+}
+
+export default UserInfo;
