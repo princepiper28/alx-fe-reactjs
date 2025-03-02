df95570ab035ab2f92811719a6cff688d739ba7e
prince joseph udoewah <princepiper28@gmail.com>
2025-03-03 00:00:43 +0100

    Fixed recipeStore.js: Added updateRecipe and deleteRecipe

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,10 +1,24 @@
 import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
-  recipes: [],
-  addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
+  recipes: [
+    { id: 1, title: "Jollof Rice", description: "A delicious Nigerian rice dish." },
+    { id: 2, title: "Egusi Soup", description: "A rich melon seed soup." }
+  ],
+
+  addRecipe: (newRecipe) => set((state) => ({
+    recipes: [...state.recipes, { id: Date.now(), ...newRecipe }]
+  })),
+
+  updateRecipe: (id, updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map(recipe =>
+      recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+    )
+  })),
+
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== id)
+  }))
 }));
 
 export default useRecipeStore;
