a5cbae6aeba9838aea6c5c513c9085ea25e427ee
prince joseph udoewah <princepiper28@gmail.com>
2025-03-07 15:49:43 +0100

    Fixed TodoList import in App.jsx

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from "react";
+import TodoList from "./TodoList";  // ✅ Ensure correct import
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Todo App</h1>
+      <TodoList />  {/* ✅ Make sure TodoList is used here */}
+    </div>
+  );
+}
+
+export default App;
