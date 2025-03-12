c5a2c53c3694b7ae9e1c3a211fb88f63f2015970
prince joseph udoewah <princepiper28@gmail.com>
2025-03-12 02:09:59 +0100

    Added md:text-xl to h1 for responsive typography

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,18 +1,18 @@
 function UserProfile() {
     return (
         <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
-        <img
-          src="https://via.placeholder.com/150"
-          alt="User"
-          className="rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 mx-auto"
-        />
-        <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4 font-semibold">
-          John Doe
-        </h1>
-        <p className="text-sm sm:text-base text-gray-600">
-          Developer at Example Co. Loves to write code and explore new technologies.
-        </p>
-      </div>
+      <img
+        src="https://via.placeholder.com/150"
+        alt="User"
+        className="rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 mx-auto"
+      />
+      <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-4 font-semibold">
+        John Doe
+      </h1>
+      <p className="text-sm sm:text-base md:text-lg text-gray-600">
+        Developer at Example Co. Loves to write code and explore new technologies.
+      </p>
+    </div>
     );
   }
   
