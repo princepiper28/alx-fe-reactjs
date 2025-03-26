fd5a69b1aa4b1297be87668cee03894ab520836b
prince joseph udoewah <princepiper28@gmail.com>
2025-03-26 18:39:26 +0100

    Initial project setup for GitHub User Search Application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-bold mb-4 text-blue-600">GitHub User Search Application</h1>
+      <p>Start building your search component here!</p>
+    </div>
+  );
+};
+
+export default App;
